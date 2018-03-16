3de2f9ee45306a11e93849390d5edde309b3c3c1
Norfolk, George (LNG-CON) <NORFOLGX@legal.regn.net>
2018-03-16 16:13:42 +0000

    First testable cookbook!

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+execute 'curl node' do
+  command 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -'
+end
+
+execute 'install node' do
+  command 'sudo apt-get install -y nodejs'
+end
